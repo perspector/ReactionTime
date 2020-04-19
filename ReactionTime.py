d06061295524a3a5948265800cefd14bce361764
Benny <bennypythoncoder@gmail.com>
2020-04-19 14:37:40 -0400

    Update ReactionTime.py

--- ReactionTime.py
+++ ReactionTime.py
@@ -1,17 +1,13 @@
 import time
 import random
-import gpiozero
-led = gpiozero.LED(26)
-led.off()
+
 while True:
-    print("When The LED Lights Up (and the prompt tells you to), Press ENTER On Your Keyboard!\n")
+    print("When the prompt tells you to, press ENTER On Your Keyboard!\n")
     number = random.randint(2, 6)
     time.sleep(number)
-    led.on()
     start = time.time()
     input("PRESS ENTER!!! ")
     end = time.time()
-    led.off()
     reaction_time = end - start
     rounded = round(reaction_time, 5)
     input("It took you "+str(reaction_time)+"(About "+str(rounded)+" sec) seconds to respond! Press ENTER to play again! ")
